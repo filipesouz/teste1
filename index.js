34e2e48659883fcfdb50bb84e05a88d5a4d1dece
Tiago Amaral <tiago.amaral@infoglobo.com.br>
2021-05-12 00:09:41 -0300

    Fiz umas alteracoes bem legais, da uma olhada

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var app = express();
-// var livereload = require('easy-livereload');
-   
-app.use(require('easy-livereload')());
+
 app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.send('lorem ipsum dolor');
 });
 
-
-
 app.listen(3000, () => {
-
     console.log('Server escutando porta 3000');
 });
